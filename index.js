7bf6a064f9a192e76d152ed83a0d9003e9e15801
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-04 20:31:28 +0100

    Updated JS to include background blend mode

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function openNav(){
     let setWindowSize = "75%"
-    window.innerHeight >= 750 ? setWindowSize = "25%" : setWindowSize = "75%"
+    window.innerHeight >= 750 ? setWindowSize = "50%" : setWindowSize = "75%"
     document.getElementById("sidemenu").style.width = setWindowSize;
+    document.querySelector("main").style.backgroundBlendMode = "darken";
 }
 function closeNav(){
     document.getElementById("sidemenu").style.width = "0%";
+    document.querySelector("main").style.backgroundBlendMode = "normal";
 }
