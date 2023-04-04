3cdb87394ffea9e1c997355a143c48de5080422a
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-04 20:19:00 +0100

    Updated JS to change menubar width based on screensize

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function openNav(){
-    document.getElementById("sidemenu").style.width = "75%";
+    let setWindowSize = "75%"
+    window.innerHeight >= 750 ? setWindowSize = "25%" : setWindowSize = "75%"
+    document.getElementById("sidemenu").style.width = setWindowSize;
 }
 function closeNav(){
     document.getElementById("sidemenu").style.width = "0%";
