a14d5a8b7bea93b847f689fffa36db699f43706d
Liam Hebblewhite <l.hebblewhite1@gmail.com>
2023-04-04 21:01:57 +0100

    Multiple updates. HTML updated to include divs for shadow area. CSS styles created. JS function updated.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function openNav(){
-    let setWindowSize = "75%"
-    window.innerHeight >= 750 ? setWindowSize = "50%" : setWindowSize = "75%"
-    document.getElementById("sidemenu").style.width = setWindowSize;
-    document.querySelector("main").style.backgroundBlendMode = "darken";
+    let setContentSize = "75%"
+    let setShadowSize = "25%"
+    window.innerHeight >= 750 ? {setContentSize: "50%", setShadowSize: "50%"}: {setContentSize: "75%", setShadowSize: "25%"}
+    document.getElementById("sidemenu").style.width = "100%";
+    document.getElementById("sidemenuContent") = setContentSize;
+    document.getElementById("sidemenuShadowArea") = setShadowSize;
 }
 function closeNav(){
     document.getElementById("sidemenu").style.width = "0%";
